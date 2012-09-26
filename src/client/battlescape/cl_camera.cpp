4921e42f7402ff5b35adc1afc6814ed52027430c
Ian Wallis <ian.wallis@grapplemobile.com>
2012-09-26 08:54:42 +0100

    Fixed save camera header file

--- src/client/battlescape/cl_camera.cpp
+++ src/client/battlescape/cl_camera.cpp
@@ -31,7 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../input/cl_input.h"
 #include "events/e_parse.h"
 #include "../../common/xml.h"
-#include "../cgame/campaign/save/save_bs_localentitiy.h"
+#include "../cgame/campaign/save/save_bs_camera.h"
 
 
 static bool cameraRoute = false;
---
+++ src/client/cgame/campaign/save/save_bs_camera.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   save_bs_camera.h
+ * Author: wallisi
+ *
+ * Created on September 26, 2012, 8:52 AM
+ */
+
+#ifndef SAVE_BS_CAMERA_H
+#define	SAVE_BS_CAMERA_H
+
+#define SAVE_CAMERA "camera"
+
+#endif	/* SAVE_BS_CAMERA_H */
+
